8ade625ea74f0a51cb4289217213f85ce1cc6aa9
ChrysanthosM <ChrysanthosM@Outlook.com>
2024-02-04 16:17:15 +0200

    MockMvc

---
+++ src/test/java/j2q/tests/TestRestAutoNumberingController.java
@@ -0,0 +1,29 @@
+package j2q.tests;
+
+import j2q.setup.controllers.RestAutoNumberingController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(RestAutoNumberingController.class)
+public class TestRestAutoNumberingController {
+    private @Autowired MockMvc mockMvc;
+
+    @Test
+    public void testGetAutoNumberingList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/AutoNumberingList?type=ALL"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+    }
+
+    @Test
+    public void testGetAutoNumberingListAsync() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/AutoNumberingListAsync?type=ALL"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+    }
+
+}
---
+++ src/test/java/j2q/tests/TestRestControllerSQL.java
@@ -0,0 +1,23 @@
+package j2q.tests;
+
+import j2q.setup.controllers.RestControllerSQL;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(RestControllerSQL.class)
+public class TestRestControllerSQL {
+    private @Autowired MockMvc mockMvc;
+
+    @Test
+    public void testGetAutoNumberingSQL() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/AutoNumberingSQL?type=ALL"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(is(equalToCompressingWhiteSpace("SELECT Sys_RecID, Sys_EntityType, Sys_EntityNumber   FROM Sys_AutoNumbering"))
+                ));
+    }
+}
